d761456eea01484dca8673e805af384076d72ae3
Oskar Krawczyk <oskar@krawczyk.io>
2020-02-20 20:56:26 +0100

    Filters are not auto-generated from the product JSON

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,20 +1,92 @@
 class Filter {
 
-  constructor(galleryElements){
-    this.filters = document.querySelectorAll("#filters select")
+  constructor(data){
+    this.filters = document.querySelector("#filters .fieldset")
     this.reset   = document.querySelector("#filters input[type=reset]")
     this.results = document.querySelector("#results")
+    this.data    = data
 
-    Array.from(this.filters).forEach((filter) => {
-      filter.addEventListener("change", (event) => {
-        history.pushState({}, "", this.getSelections())
-        this.filterProducts()
-      })
-    })
+    // Array.from(this.filters).forEach((filter) => {
+    //   filter.addEventListener("change", (event) => {
+    //     history.pushState({}, "", this.getSelections())
+    //     this.filterProducts()
+    //   })
+    // })
 
     // initial lisitng of all products
+    this.buildFilters()
     this.filterProducts()
     this.preselectFilters()
+  }
+
+  buildFilters(){
+    let ignore = ["Index", "Nazwa", "Zdjęcie produktu", "Data (kiedy możemy to pokazać)"]
+    window.store = {}
+    let selects = {}
+
+    Object.keys(this.data[0]).forEach((filter) => {
+      if (ignore.indexOf(filter) < 0){
+
+        let p = document.createElement("p")
+        p.innerHTML = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9" /></svg>`
+
+        let label = document.createElement("label")
+        label.innerText = filter
+
+        let select = document.createElement("select")
+        select.name = filter
+        select.addEventListener("change", (event) => {
+          event.preventDefault()
+          event.stopPropagation()
+          
+          history.pushState({}, "", this.getSelections())
+          this.filterProducts()
+        })
+
+        p.appendChild(label)
+        p.appendChild(select)
+        this.filters.appendChild(p)
+
+        selects[filter] = select
+        window.store[filter] = []
+
+        Object.keys(this.data).forEach((item) => {
+          this.ignoreExisting(window.store[filter], this.data[item][filter])
+        })
+      }
+    })
+
+    // special case for colors
+    window.store["Kolor"] = window.store["Kolor"].join(", ")
+    window.store["Kolor"] = window.store["Kolor"].split(", ")
+
+    let tempColorStorage = []
+    Object.keys(window.store["Kolor"]).forEach((color) => {
+      if (tempColorStorage.indexOf(window.store["Kolor"][color]) < 0){
+        tempColorStorage.push(window.store["Kolor"][color])
+      }
+    })
+    window.store["Kolor"] = tempColorStorage
+
+    Object.keys(selects).forEach((selectFilter) => {
+      let option = document.createElement("option")
+      option.value = ""
+      option.innerText = ""
+      selects[selectFilter].appendChild(option)
+
+      window.store[selectFilter].forEach((item) => {
+        let option = document.createElement("option")
+        option.value = item
+        option.innerText = item
+        selects[selectFilter].appendChild(option)
+      })
+    })
+  }
+
+  ignoreExisting(arr, item){
+    if (arr.indexOf(item) < 0){
+      return arr.push(item)
+    }
   }
 
   getSelections(){
@@ -26,6 +98,7 @@ class Filter {
         out.push(`${el.name}=${value}`)
       }
     })
+
     return `#${out.join("&")}`
   }
 
@@ -65,7 +138,7 @@ class Filter {
   }
 
   filterProducts(products){
-    let filteredResults = SEARCHJS.matchArray(data, this.decodeParams())
+    let filteredResults = SEARCHJS.matchArray(this.data, this.decodeParams())
     this.results.innerHTML = this.buildGallery(filteredResults)
   }
 
@@ -73,8 +146,8 @@ class Filter {
     let output = []
     products.forEach((product) => {
       output.push(`<li>
-        <a href="#">
-          <img src="public/images/products/${product.Index}.jpg">
+        <a href="public/images/products/${product.Index}.png" target="_blank">
+          <img src="public/images/products/${product.Index}.png">
           <h3>${product.Nazwa}</h3>
           <section>
             <em>${product.Sezon}</em>
@@ -86,7 +159,3 @@ class Filter {
     return output.join("")
   }
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  window.TealManager = new Filter()
-})
